1dc35f14a9370fabebb6260be4b43272993e3c5b
kaufman79 <kaufman79@gmail.com>
2024-06-12 14:10:00 -0400

    changes directory and variables to more generic names (since this can be repurposed for other uses)

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from fpdf import FPDF
 from pathlib import Path
 import glob
 
-filepaths = glob.glob("animals/*.txt")
+filepaths = glob.glob("files/*.txt")
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 for filepath in filepaths:
     pdf.add_page()
     pdf.set_font(family="Times", style="B", size=16)
-    animal = Path(filepath).stem
-    pdf.cell(w=50, h=12, txt=animal.title(), align="L",
+    name = Path(filepath).stem
+    pdf.cell(w=50, h=12, txt=name.title(), align="L",
              ln=1, border=0)
 
-pdf.output("animal descriptions.pdf")
+pdf.output("output.pdf")
